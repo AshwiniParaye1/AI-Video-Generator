4c714528577b7e9f46f1fd53bbdbee9d972fe5c5
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-03 16:31:45 +0530

    fixed shadcn issue

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h2>AI Video Generator</h2>
+      <Button>Button</Button>
       <UserButton />
     </div>
   );
