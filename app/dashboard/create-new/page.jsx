24a197817ba5f35311d58a9514096e5c8b2ebf1b
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-09 15:52:17 +0530

    generate caption

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -13,11 +13,15 @@ import { v4 as uuidv4 } from "uuid";
 const scriptData =
   "Imagine a world without wheels. That's what our ancestors lived with. Ancient civilizations built empires, invented writing, and shared knowledge. From knights and castles to the rise of powerful kingdoms, history is full of fascinating stories. The pursuit of knowledge and discovery has driven human progress for centuries. History connects us to our past and shapes our future. By studying history, we gain valuable insights into ourselves and the world around us.";
 
+const FileUrl =
+  "https://firebasestorage.googleapis.com/v0/b/ai-video-generator-f17f5.appspot.com/o/ai-short-video-files%2F%2B2f98fd63-8e32-4f9a-96a5-9cc3e1e3db0a.mp3?alt=media&token=0f58f42d-cbfb-44f5-b0d3-09dc38b900fa";
+
 function CreateNew() {
   const [formData, setFormData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [videoScript, setVideoScript] = useState();
   const [audioFileUrl, setAudioFileUrl] = useState();
+  const [captions, setCaptions] = useState();
 
   const handleInputChange = (fieldName, fieldValue) => {
     console.log(" Value==", fieldValue);
@@ -32,7 +36,9 @@ function CreateNew() {
 
   const onCreateClickHandler = () => {
     // GetVideoScript();
-    GenerateAudioFile(scriptData);
+    // GenerateAudioFile(scriptData);
+
+    GenerateAudioCaption(FileUrl);
   };
   //get video script
   const GetVideoScript = async () => {
@@ -80,6 +86,21 @@ function CreateNew() {
       .then((res) => {
         console.log("res===", res.data.res);
         setAudioFileUrl(res.data.result);
+      });
+
+    setLoading(false);
+  };
+
+  const GenerateAudioCaption = async (fileUrl) => {
+    setLoading(true);
+
+    await axios
+      .post("/api/generate-caption", {
+        audioFileUrl: fileUrl
+      })
+      .then((res) => {
+        console.log("caption res===", res.data.result);
+        setCaptions(res?.data?.result);
       });
 
     setLoading(false);
