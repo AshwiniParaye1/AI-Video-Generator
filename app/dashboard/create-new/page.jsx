fda4170699a4c736b3955fc941bd33a47d569469
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-10 17:29:05 +0530

    updated checks

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import SelectTopic from "./_components/SelectTopic";
 import SelectStyle from "./_components/SelectStyle";
 import SelectDuration from "./_components/SelectDuration";
@@ -8,22 +8,7 @@ import { Button } from "@/components/ui/button";
 import axios from "axios";
 import CustomLoading from "./_components/CustomLoading";
 import { v4 as uuidv4 } from "uuid";
-
-//FOR TEST
-const scriptData =
-  "Imagine a world without wheels. That's what our ancestors lived with. Ancient civilizations built empires, invented writing, and shared knowledge. From knights and castles to the rise of powerful kingdoms, history is full of fascinating stories. The pursuit of knowledge and discovery has driven human progress for centuries. History connects us to our past and shapes our future. By studying history, we gain valuable insights into ourselves and the world around us.";
-
-const FileUrl =
-  "https://firebasestorage.googleapis.com/v0/b/ai-video-generator-f17f5.appspot.com/o/ai-short-video-files%2F%2B2f98fd63-8e32-4f9a-96a5-9cc3e1e3db0a.mp3?alt=media&token=0f58f42d-cbfb-44f5-b0d3-09dc38b900fa";
-
-const VideoScriptData = [
-  {
-    imagePrompt:
-      "A bustling medieval marketplace, full of colorful stalls and people haggling over goods. The sun is shining brightly and the air is filled with the sounds of chatter and bartering.",
-    contentText:
-      "In the heart of 14th century Florence, a young artist named Donatello was struggling to make ends meet. He was known for his talent, but his sculptures were often overlooked in the shadow of the city's established masters."
-  }
-];
+import { VideoDataContext } from "@/app/_context/VideoDataContext";
 
 function CreateNew() {
   const [formData, setFormData] = useState([]);
@@ -32,6 +17,7 @@ function CreateNew() {
   const [audioFileUrl, setAudioFileUrl] = useState();
   const [captions, setCaptions] = useState();
   const [imageList, setImageList] = useState([]);
+  const { videoData, setVideoData } = useContext(VideoDataContext);
 
   const handleInputChange = (fieldName, fieldValue) => {
     console.log(" Value==", fieldValue);
@@ -65,17 +51,17 @@ function CreateNew() {
       formData.imageStyle +
       " format for each scene and give me result in JSON format with imagePrompt and ContentText as field, No Plain text";
 
-    const result = await axios
+    await axios
       .post("/api/get-video-script", {
         prompt: prompt
       })
-      .then((res) => {
+      .then(async (res) => {
+        setVideoData((prev) => ({ ...prev, videoScript: res.data.result }));
+
         setVideoScript(res.data.result);
 
-        GenerateAudioFile(res.data.result);
+        await GenerateAudioFile(res.data.result);
       });
-
-    setLoading(false);
   };
 
   //generate audio file and save in firebase storage
@@ -90,16 +76,20 @@ function CreateNew() {
       script += item.contentText + " ";
     });
 
-    const res = await axios.post("/api/generate-audio", {
-      text: script,
-      id: id
-    });
+    await axios
+      .post("/api/generate-audio", {
+        text: script,
+        id: id
+      })
+      .then(async (res) => {
+        setVideoData((prev) => ({ ...prev, audioFileUrl: res.data.result }));
 
-    setAudioFileUrl(res.data.result);
-    res.data.result &&
-      (await GenerateAudioCaption(res.data.result, videoScriptData));
+        setAudioFileUrl(res.data.result);
+        res.data.result &&
+          (await GenerateAudioCaption(res.data.result, videoScriptData));
 
-    setLoading(false);
+        setLoading(false);
+      });
   };
 
   //generate caption from audio file
@@ -111,8 +101,9 @@ function CreateNew() {
         audioFileUrl: fileUrl
       })
       .then((res) => {
-        console.log("caption res===", res.data.result);
         setCaptions(res?.data?.result);
+
+        setVideoData((prev) => ({ ...prev, captions: res.data.result }));
 
         res.data.result && GenerateImage(videoScriptData);
       });
@@ -128,16 +119,21 @@ function CreateNew() {
           prompt: element.imagePrompt
         });
 
-        console.log("image res===", res.data.result);
         images.push(res.data.result);
       } catch (error) {
         console.log(error);
         return NextResponse.json({ "Error ": error });
       }
     }
+    setVideoData((prev) => ({ ...prev, imageList: images }));
+
     setImageList(images);
     setLoading(false);
   };
+
+  useEffect(() => {
+    console.log(videoData);
+  }, [videoData]);
 
   return (
     <div className="md:px-20">
