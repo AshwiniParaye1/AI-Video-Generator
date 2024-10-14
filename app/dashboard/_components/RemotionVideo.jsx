7739db7184335d834b9c267bd999ddf4bdac202a
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-14 19:51:51 +0530

    added animation to video

--- app/dashboard/_components/RemotionVideo.jsx
+++ app/dashboard/_components/RemotionVideo.jsx
@@ -3,6 +3,7 @@ import {
   AbsoluteFill,
   Audio,
   Img,
+  interpolate,
   Sequence,
   useCurrentFrame,
   useVideoConfig
@@ -26,48 +27,69 @@ function RemotionVideo({
 
   const getCurrentCaptions = () => {
     const currentTime = (frame / 30) * 1000; // convert frame to seconds
-    const currentCaption = captions.find((word) => {
-      word.start <= currentTime && word.end >= currentTime;
-    });
+    const currentCaption = captions.find(
+      (word) => word.start <= currentTime && word.end >= currentTime
+    );
     return currentCaption ? currentCaption?.text : "";
   };
 
   return (
     script && (
       <AbsoluteFill className="bg-black">
-        {imageList?.map((image, index) => (
-          <>
-            <Sequence
-              key={index}
-              from={(index * getDurationFrames()) / imageList.length}
-              durationInFrames={getDurationFrames()}
-            >
-              <AbsoluteFill
-                style={{ justifyContent: "center", alignItems: "center" }}
+        {imageList?.map((image, index) => {
+          const startTime = (index * getDurationFrames()) / imageList.length;
+          const duration = getDurationFrames();
+
+          const scale = (index) =>
+            interpolate(
+              frame,
+              [startTime, startTime + duration / 2, startTime + duration], //zoom in and out
+              index % 2 === 0 ? [1, 1.8, 1] : [1.8, 1, 1.8],
+              {
+                extrapolateLeft: "clamp",
+                extrapolateRight: "clamp"
+              }
+            );
+
+          return (
+            <>
+              <Sequence
+                key={index}
+                from={startTime}
+                durationInFrames={getDurationFrames()}
               >
-                <Img
-                  src={image}
-                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
-                />
                 <AbsoluteFill
-                  style={{
-                    color: "white",
-                    top: undefined,
-                    height: 150,
-                    bottom: 50,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    textAlign: "center",
-                    textAlign: "center",
-                    width: "100%"
-                  }}
+                  style={{ justifyContent: "center", alignItems: "center" }}
                 >
-                  <h2 className="text-2xl">{getCurrentCaptions()}</h2>
+                  <Img
+                    src={image}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                      transform: `scale(${scale(index)})`
+                    }}
+                  />
+                  <AbsoluteFill
+                    style={{
+                      color: "white",
+                      top: undefined,
+                      height: 150,
+                      bottom: 50,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      textAlign: "center",
+                      textAlign: "center",
+                      width: "100%"
+                    }}
+                  >
+                    <h2 className="text-2xl">{getCurrentCaptions()}</h2>
+                  </AbsoluteFill>
                 </AbsoluteFill>
-              </AbsoluteFill>
-            </Sequence>
-          </>
-        ))}
+              </Sequence>
+            </>
+          );
+        })}
 
         <Audio src={audioFileUrl} />
       </AbsoluteFill>
