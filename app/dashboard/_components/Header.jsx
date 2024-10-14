2eb3b24c346abb6eb123404cfbdbc87eafcb168a
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-14 19:01:29 +0530

    change text

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
         <h2 className="font-bold text-xl">Clip AI</h2>
       </div>
       <div className="flex gap-3 items-center">
-        <Button>dashboard</Button>
+        <Button>Dashboard</Button>
         <UserButton className="bg-red-500" />
       </div>
     </div>
