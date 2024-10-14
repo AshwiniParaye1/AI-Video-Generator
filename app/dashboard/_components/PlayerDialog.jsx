2365e86297a55c234ff0f49c12367abc602b57bf
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-14 14:12:14 +0530

    dialog player added

--- app/dashboard/_components/PlayerDialog.jsx
+++ app/dashboard/_components/PlayerDialog.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "@/components/ui/dialog";
+import { Player } from "@remotion/player";
+import RemotionVideo from "./RemotionVideo";
 
 function PlayerDialog() {
-  return <div>PlayerDialog</div>;
+  return (
+    <Dialog>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-3xl font-bold my-5">
+            Your video is ready!
+          </DialogTitle>
+          <DialogDescription>
+            <Player
+              component={RemotionVideo}
+              durationInFrames={120}
+              compositionWidth={300}
+              compositionHeight={450}
+              fps={30}
+            />
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 }
 
 export default PlayerDialog;
