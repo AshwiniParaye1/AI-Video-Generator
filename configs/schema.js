d5358d03f2827919f7056b7f4c89063bec0d081a
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-16 11:37:31 +0530

    schema created for credits

--- configs/schema.js
+++ configs/schema.js
@@ -1,11 +1,19 @@
-import { json, boolean, pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import {
+  json,
+  boolean,
+  pgTable,
+  serial,
+  varchar,
+  integer
+} from "drizzle-orm/pg-core";
 
 export const Users = pgTable("users", {
   id: serial("id").primaryKey(),
   name: varchar("name").notNull(),
   email: varchar("email").notNull(),
   imageUrl: varchar("imageUrl"),
-  subscription: boolean("subscription").default(false)
+  subscription: boolean("subscription").default(false),
+  credits: integer("credits").default(30) //30 credits = 3 videos
 });
 
 export const VideoData = pgTable("videodata", {
