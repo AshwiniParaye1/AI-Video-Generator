ed33aa760779907fba917f03033e7843613788a3
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-16 11:58:14 +0530

    creadits added

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,9 +1,12 @@
+import { UserDetailsContext } from "@/app/_context/UserDetailsContext";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
-import React from "react";
+import React, { useContext } from "react";
 
 function Header() {
+  const { userDetails, setUserDetails } = useContext(UserDetailsContext);
+
   return (
     <div className="p-3 px-5 flex items-center justify-between shadow-md">
       <div className="flex gap-3 items-center">
@@ -11,6 +14,10 @@ function Header() {
         <h2 className="font-bold text-xl">Clip AI</h2>
       </div>
       <div className="flex gap-3 items-center">
+        <div className="flex gap-2 items-center">
+          <Image src={"/credits.png"} alt="credits" width={30} height={30} />
+          <h2>{userDetails?.credits}</h2>
+        </div>
         <Button>Dashboard</Button>
         <UserButton className="bg-red-500" />
       </div>
