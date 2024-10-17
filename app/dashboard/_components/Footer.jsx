89c576954b959ac69b9d617a14d090547b1f8559
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-17 15:57:35 +0530

    modified fotter style

--- app/dashboard/_components/Footer.jsx
+++ app/dashboard/_components/Footer.jsx
@@ -1,20 +1,51 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import {
+  EnvelopeOpenIcon,
+  GitHubLogoIcon,
+  LinkedInLogoIcon
+} from "@radix-ui/react-icons";
 import Link from "next/link";
 import React from "react";
 
 function Footer() {
   return (
-    <div className="p-3 w-full px-5 flex items-center justify-between shadow-md">
-      <Link href={"/"}>
-        <div className="flex gap-3 items-center">
-          <Image src={"/logo.png"} alt="logo" width={30} height={30} />
-          <h2 className="font-bold text-xl">Clip AI</h2>
-        </div>
-      </Link>
-      <div className="flex gap-3 items-center">
-        <UserButton />
+    <div className="py-4 px-6 flex justify-between items-center">
+      {/* Copyright Section */}
+      <div className="text-gray-400">
+        © {new Date().getFullYear()} Clip AI. All rights reserved.
+      </div>
+
+      {/* Made by Section with Contact and Social Media Icons */}
+      <div>
+        <p className="text-gray-400">
+          Made with ❤️ by{" "}
+          <span className="font-bold">
+            <Link href={"https://www.linkedin.com/in/ashwini-paraye/"}>
+              Ashwini Paraye
+            </Link>
+          </span>
+        </p>
+      </div>
+
+      {/* Contact and Social Media Icons */}
+      <div className="flex gap-4">
+        <Link
+          href="mailto:ashwiniparaye9309@gmail.com"
+          className="text-gray-400 hover:text-gray-500 transition"
+        >
+          <EnvelopeOpenIcon className="w-5 h-5" />
+        </Link>
+        <Link
+          href={"https://github.com/AshwiniParaye1"}
+          className="text-gray-400 hover:text-gray-500 transition"
+        >
+          <GitHubLogoIcon className="w-5 h-5" />
+        </Link>
+        <Link
+          href={"https://www.linkedin.com/in/ashwini-paraye/"}
+          className="text-gray-400 hover:text-gray-500 transition"
+        >
+          <LinkedInLogoIcon className="w-5 h-5" />
+        </Link>
       </div>
     </div>
   );
