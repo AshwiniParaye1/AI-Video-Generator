6aa4977cfc151db14ffc000c947af00fac945422
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-17 16:46:08 +0530

    add video to bg

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,20 +1,24 @@
 import { SignIn } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
-      <div>
-        <Image
-          src="/login.jpeg"
-          alt="login"
-          width={500}
-          height={500}
-          className="w-full object-contain"
-        />
-      </div>
-      <div className="flex justify-center items-center h-screen">
-        <SignIn />
+    <div className="relative min-h-screen overflow-hidden">
+      {/* Background video */}
+      <video
+        src="./Gen3Promo.mp4"
+        autoPlay
+        loop
+        className="absolute top-0 left-0 w-full h-full object-cover filter"
+      />
+
+      {/* Overlay for better visibility */}
+      <div className="absolute inset-0 bg-black opacity-30" />
+
+      {/* Centered sign-in form */}
+      <div className="flex justify-center items-center min-h-screen relative z-10">
+        <div className="flex justify-center items-center h-screen">
+          <SignIn />
+        </div>
       </div>
     </div>
   );
