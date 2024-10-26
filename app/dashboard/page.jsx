2d598ba7539fa3eef307dd7032688ce092620f83
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-26 17:22:44 +0530

    add margin top

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -30,23 +30,23 @@ function Dashboard() {
   };
 
   return (
-    <div>
-      <div className="flex justify-between items-center">
+    <div className="mt-16">
+      {" "}
+      {/* Added padding to the main container */}
+      <div className="flex justify-between items-center mb-4">
+        {" "}
+        {/* Added margin-bottom */}
         <h2 className="font-bold text-2xl text-primary">Dashboard</h2>
         <Link href={"/dashboard/create-new"}>
           <Button>+ Create New</Button>
         </Link>
       </div>
-
       {/* Empty state */}
-      {videoList.length === 0 && (
-        <div>
-          <EmptyState />
-        </div>
+      {videoList.length === 0 ? (
+        <EmptyState />
+      ) : (
+        <VideoList videoList={videoList} />
       )}
-
-      {/* Video list */}
-      <VideoList videoList={videoList} />
     </div>
   );
 }
