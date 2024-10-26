cd2d90c94d766c719bd6f4446a786cea5c020a57
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-26 16:19:16 +0530

    update header for smaller devices

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -6,25 +6,23 @@ import Link from "next/link";
 import React, { useContext } from "react";
 
 function Header() {
-  const { userDetails, setUserDetails } = useContext(UserDetailsContext);
+  const { userDetails } = useContext(UserDetailsContext);
 
   return (
-    <div className="p-3 px-5 flex items-center justify-between shadow-md">
-      <Link href={"/"}>
-        <div className="flex gap-3 items-center">
-          <Image src={"/logo.png"} alt="logo" width={30} height={30} />
-          <h2 className="font-bold text-xl">Clip AI</h2>
-        </div>
+    <div className="p-3 px-5 flex items-center justify-between shadow-md flex-wrap">
+      <Link href={"/"} className="flex items-center gap-3">
+        <Image src={"/logo.png"} alt="logo" width={30} height={30} />
+        <h2 className="font-bold text-xl">Clip AI</h2>
       </Link>
-      <div className="flex gap-3 items-center">
-        <div className="flex gap-2 items-center">
+      <div className="flex items-center gap-3 mt-2 md:mt-0">
+        <div className="flex gap-2 items-centerrounded-lg p-2">
           <Image src={"/credits.png"} alt="credits" width={30} height={30} />
           <h2>{userDetails?.credits}</h2>
         </div>
         <Link href={"/dashboard"}>
-          <Button>Dashboard</Button>
+          <Button className="hidden md:block">Dashboard</Button>
         </Link>
-        <UserButton className="bg-red-500" />
+        <UserButton />
       </div>
     </div>
   );
